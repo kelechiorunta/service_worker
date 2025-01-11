e40d1ba8dcbfea23fe1ac6658a715481e847ee47
Kelechi Orunta <kelechiorunta1@gmail.com>
2025-01-11 10:06:03 +0100

    Updated the service worker with delete of old caches

--- main.js
+++ main.js
@@ -3,21 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const registerServiceWorker = async(scriptUrl) => {
         if ('serviceWorker' in navigator && navigator.serviceWorker.ready) {
-            const registeration = await navigator.serviceWorker.register(scriptUrl, {scope: '/'});
+            const registeration = await navigator.serviceWorker.register(scriptUrl, {scope: './'});
             try{
                 if (registeration && registeration.installing) {
-                    const frame = document.querySelector('.frame');
-                    const img = frame.querySelector('img');
                     
-                    const loaded = () => {
-                        frame.classList.add('loaded')
-                    }
-                
-                    if (img.complete) {
-                        loaded()
-                    }
-                    img.addEventListener('load', loaded)
-                    frame.style.setProperty('background-image', 'url(/fullImg)')
                     
                     console.log("Service worker installed")
                 }else if (registeration && registeration.active) {
@@ -37,7 +26,46 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     
+    const frame = document.querySelector('.frame');
+    const img = frame.querySelector('img');
+                    
+        const loaded = () => {
+            frame.classList.add('loaded')
+        }
+                
+        if (img.complete) {
+            loaded()
+        }
+        img.addEventListener('load', loaded)
+        frame.style.setProperty('background-image', 'url(/fullImg)')
 
+    const intersectingOptions = {
+        root: null,
+        margin: '2px',
+        threshold: 1
+    }
+
+    let yPos = 0
+
+    const pageIntersection = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                console.log(entry.target)
+                // if (entry.target.classList.contains('img')) {
+                entry.target.classList.add('fall')
+                    // entry.target.style.setProperty('transform', `translateY(${20}px)`)
+                    // entry.target.parentElement.style.setProperty('background-position-y', `${yPos}px`)
+                // }
+            }
+        })
+    }
+
+    const pageObserver = new IntersectionObserver(pageIntersection, intersectingOptions);
+
+    // (document.body.children).forEach(child => {
+        const child = document.querySelectorAll('.frame img')[0];
+        pageObserver.observe(child)
+    // })
     
 
     registerServiceWorker('./service_worker.js');
